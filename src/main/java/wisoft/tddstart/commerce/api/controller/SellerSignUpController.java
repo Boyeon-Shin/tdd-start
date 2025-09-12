e787efdc69ccd3422fa0ecc4670c315f969d3eb9
Boyeon-Shin <anna007096@gmail.com>
2025-09-12 17:14:07 +0900

    refactor: isCommandValid 메서드 추출

--- src/main/java/wisoft/tddstart/commerce/api/controller/SellerSignUpController.java
+++ src/main/java/wisoft/tddstart/commerce/api/controller/SellerSignUpController.java
@@ -8,23 +8,36 @@ import wisoft.tddstart.commerce.command.CreateSellerCommand;
 
 @RestController
 public record SellerSignUpController() {
+    public static final String EMAIL_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$";
+    public static final String USERNAME_REGEX = "[a-zA-Z0-9_-]*$";
+
 
     @PostMapping("/seller/signUp")
     ResponseEntity<?> signUp(@RequestBody CreateSellerCommand command) {
-        String emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$";
-        String usernameRegex = "^[a-zA-Z0-9_-]*$";
-
-        if (command.email() != null && command.email().contains("@") != false && !command.email().endsWith("@")
-                && command.email().matches(emailRegex) != false && command.username() != null && !command.username()
-                .isBlank() && command.username().length() >= 3 && command.username().matches(usernameRegex) != false
-                && command.password() != null) {
-            if (command.password().length() < 8) {
-                return ResponseEntity.badRequest().build();
-            } else {
-                return ResponseEntity.noContent().build();
-            }
+        if (isCommandValid(command)) {
+            return ResponseEntity.noContent().build();
         } else {
-    return ResponseEntity.badRequest().build();
-}
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    private static boolean isCommandValid(final CreateSellerCommand command) {
+        String email = command.email();
+        boolean emailValid = email != null
+                && email.contains("@")
+                && !email.endsWith("@")
+                && email.matches(EMAIL_REGEX);
+
+        String username = command.username();
+        boolean userNameValid = username != null
+                && !username.isBlank()
+                && username.length() >= 3
+                && username.matches(USERNAME_REGEX);
+
+        String password = command.password();
+        boolean passwordValid = password != null
+                && password.length() >= 8;
+
+        return emailValid && userNameValid && passwordValid;
     }
 }
