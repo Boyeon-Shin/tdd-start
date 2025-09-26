6c1a4fa1b5f7f84ebfda9033b758cc84da6761c7
Boyeon-Shin <anna007096@gmail.com>
2025-09-26 14:40:45 +0900

    HTTP 응답 본문 - 올바르게 요청하면 접근 토큰을 반환한다

--- src/main/java/wisoft/tddstart/commerce/api/controller/SellerIssueTokenController.java
+++ src/main/java/wisoft/tddstart/commerce/api/controller/SellerIssueTokenController.java
@@ -2,12 +2,13 @@ package wisoft.tddstart.commerce.api.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import wisoft.tddstart.commerce.result.AccessTokenCarries;
 
 @RestController
 public class SellerIssueTokenController {
 
     @PostMapping("/seller/issueToken")
-    void issueToken() {
-
+    AccessTokenCarries issueToken() {
+        return new AccessTokenCarries("token");
     }
 }
--- src/test/java/wisoft/tddstart/commerce/issueToken/POST_specs.java
+++ src/test/java/wisoft/tddstart/commerce/issueToken/POST_specs.java
@@ -21,15 +21,15 @@ import wisoft.tddstart.commerce.result.AccessTokenCarries;
 )
 @DisplayName("POST /seller/issueToken")
 public class POST_specs {
-    
+
     @Test
-    void 올바르게_요청하면_200_OK_상태코드를_반환한다 (@Autowired TestRestTemplate client) {
+    void 올바르게_요청하면_200_OK_상태코드를_반환한다(@Autowired TestRestTemplate client) {
         String email = generateEmail();
         String password = generatePassword();
 
         //Arrange
         client.postForEntity("/seller/signUp",
-                new CreateSellerCommand(email, generateUsername(),password),
+                new CreateSellerCommand(email, generateUsername(), password),
                 Void.class);
 
         //Act
@@ -41,4 +41,53 @@ public class POST_specs {
         //Assert
         Assertions.assertThat(response.getStatusCode().value()).isEqualTo(200);
     }
+
+
+    @Test
+    void 올바르게_요청하면_접근_토큰을_반환한다(@Autowired TestRestTemplate client) {
+        String email = generateEmail();
+        String password = generatePassword();
+
+        //Arrange
+        client.postForEntity("/seller/signUp",
+                new CreateSellerCommand(email, generateUsername(), password),
+                Void.class);
+
+        //Act
+        ResponseEntity<AccessTokenCarries> response = client.postForEntity(
+                "/seller/issueToken",
+                new IssueSellerToken(email, password),
+                AccessTokenCarries.class);
+
+        //Assert
+        Assertions.assertThat(response.getBody()).isNotNull();
+        Assertions.assertThat(response.getBody().accessToken()).isNotNull();
+    }
+
+
+    void 접근_토큰은_JWT_형식을_따른다(@Autowired TestRestTemplate client) {
+
+        String email = generateEmail();
+        String password = generatePassword();
+
+        //Arrange
+        client.postForEntity("/seller/signUp",
+                new CreateSellerCommand(email, generateUsername(), password),
+                Void.class);
+
+        //Act
+        ResponseEntity<AccessTokenCarries> response = client.postForEntity(
+                "/seller/issueToken",
+                new IssueSellerToken(email, password),
+                AccessTokenCarries.class);
+
+        //Assert
+        Assertions.assertThat(response.getBody()).isNotNull();
+        Assertions.assertThat(response.getBody().accessToken()).isNotNull();
+        Assertions.assertThat(response.getBody().accessToken()).contains(".");
+    }
 }
+
+
+
+
